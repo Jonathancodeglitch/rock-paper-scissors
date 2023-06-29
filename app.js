1a22792e8bad7d8f2a8d2f9734a99b0d5fd2592f
Jonathan <jonathankendrick697@gmail.com>
2023-06-29 15:21:00 -0700

    Get player choice
    created a functionality that gets the player round and also implemeted a function that tranform the case of the player input,
    so that i can get a standard casing from the user.

--- app.js
+++ app.js
@@ -7,15 +7,26 @@ function getComputerChoice(){
     return computerChoice
 }
 
-console.log(getComputerChoice());
+getComputerChoice()
 
 //create a function that get user choice
+function getPlayerChoice(){
+    let userChoice=prompt("Rock,Paper,Scissors");
+    return changePlayerChoiceCase(userChoice)
+};
+
+getPlayerChoice()
 
 //make user choice case sensitive
+function changePlayerChoiceCase(userChoice){
+    return `${userChoice.charAt(0).toUpperCase()}${userChoice.slice(1).toLowerCase()}`
+};
 
-//create a function that takes the computer and user chioce called playRound
+//create a function that takes the computer and user chioce called playRound, playRound should look for a winner and return a string
+ 
+function playRound(){
 
-//playRound should look for a winner and return a string
+};
 
 //create a function that plays 5 rounds and keep scores and report the loser or winner.
 
