15bcf4d149eb63e1ee949c3b2b4e0844e629f89b
Jonathan <jonathankendrick697@gmail.com>
2023-06-30 02:40:51 -0700

    Play one round of game

--- app.js
+++ app.js
@@ -7,15 +7,11 @@ function getComputerChoice(){
     return computerChoice
 }
 
-getComputerChoice()
-
 //create a function that get user choice
 function getPlayerChoice(){
     let userChoice=prompt("Rock,Paper,Scissors");
     return changePlayerChoiceCase(userChoice)
 };
-
-getPlayerChoice()
 
 //make user choice case sensitive
 function changePlayerChoiceCase(userChoice){
@@ -24,9 +20,41 @@ function changePlayerChoiceCase(userChoice){
 
 //create a function that takes the computer and user chioce called playRound, playRound should look for a winner and return a string
  
-function playRound(){
+function playRound(playerChoice, computerChoice){
 
+    if(playerChoice===computerChoice){
+        console.log("Draw!!!")
+    }
+    
+    else if(playerChoice=="Rock" && computerChoice=="Scissors"){
+        console.log("You Win! Rock beats Scissors")
+    }
+
+    else if(playerChoice=="Scissors" && computerChoice=="Rock"){
+        console.log("You Lose! Rock beats Scissors")
+    }
+
+    else if(playerChoice=="Paper" && computerChoice=="Rock"){
+        console.log("You Win! Paper beats Rock")
+    }
+
+    else if(playerChoice=="Rock" && computerChoice=="Paper"){
+        console.log("You Lose! Paper beats Rock")
+    }
+
+    else if(playerChoice=="Scissors" && computerChoice=="Paper"){
+        console.log("You Win! Scissors beats Paper")
+    }
+
+    else if(playerChoice=="Paper" && computerChoice=="Scissors"){
+        console.log("You Lose! Scissors beats Paper")
+    }
+    else{
+        console.log("Invalid input,chose from rock paper scissors")
+    }
 };
+
+playRound(getPlayerChoice(),getComputerChoice());
 
 //create a function that plays 5 rounds and keep scores and report the loser or winner.
 
